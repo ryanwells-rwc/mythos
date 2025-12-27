6509517121d2f2aaf7a8655e7c6d6e44f5bec1d9
Ryan Wells <ryanwellsconsulting@gmail.com>
2025-12-27 11:55:12 -0800

    Finish hamburger menu.

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("hello world");
+const main = document.querySelector("#main");
+const footer = document.querySelector("#footer");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const menuTopNav = document.querySelector("#menuTopNav");
+const breakpoint = window.matchMedia("(width < calc(600 / 16 * 1rem))");
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+const animateMenu = () => {
+  menuTopNav.classList.add("animating");
+  setTimeout(() => {
+    menuTopNav.classList.remove("animating");
+  }, 300);
+};
+
+const setupTopNav = () => {
+  if (breakpoint.matches) {
+    //console.log("is mobile");
+    menuTopNav.setAttribute("inert", "");
+  } else {
+    //console.log("is tablet/desktop");
+    closeMobileMenu();
+    menuTopNav.removeAttribute("inert");
+  }
+};
+
+setupTopNav();
+
+function openMobileMenu() {
+  //console.log("openMobileMenu");
+  btnOpen.setAttribute("aria-expanded", "true");
+  menuTopNav.removeAttribute("inert");
+  main.setAttribute("inert", "");
+  footer.setAttribute("inert", "");
+  animateMenu();
+  bodyScrollLockUpgrade.disableBodyScroll(menuTopNav);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  //console.log("closeMobileMenu");
+  btnOpen.setAttribute("aria-expanded", "false");
+  menuTopNav.setAttribute("inert", "");
+  main.removeAttribute("inert");
+  footer.removeAttribute("inert");
+  animateMenu();
+  bodyScrollLockUpgrade.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+}
+
+breakpoint.addEventListener("change", () => {
+  //console.log("breakpoint crossed");
+  setupTopNav();
+});
